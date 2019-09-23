2db6fc3ec62f38baf40d3721088b4b936a2b99f9
jjangra2 <jjangra2@gmail.com>
2019-09-23 07:00:07 +0530

    Busschedule plugin changed

--- jarviscli/plugins/busschedule.py
+++ jarviscli/plugins/busschedule.py
@@ -1,38 +1,22 @@
 from plugin import LINUX, PYTHON3, plugin, require
-import tabula
 import time as t
 @require(python=PYTHON3, platform=LINUX)
 @plugin('busschedule')
-def convert(s): 
-  
-    # initialization of string to "" 
-    new = "" 
-  
-    # traverse in the string  
-    for x in s: 
-        new += x  
-  
-    # return string  
-    return new 
-
 def busschedule(jarvis, s):
-     df2 = tabula.read_pdf("https://www.iitmandi.ac.in/files/Institute_Vehicle_Schedule_%2030jul2019.pdf", pages=2, multiple_tables=True)
      """
      Provides latest bus available
      """
-     print("Hello")
-     arr=df2[0].get_values()
-     print(arr[8][3])
      ar=t.ctime().split(' ')
      systime=ar[3].split(':')
-     flag=0
-     for i in range(0,arr.shape[0]):
-        for j in range(3):
-            time=str(arr[i][3])
-            arr3=time.split(':')
-            if( float(systime[0]) == float(arr3[0])): 
-                if( float(systime[1]) >= float(arr3[1])):
-                    print("next bus"+arr[i][3])
-                    flag=1
-        if(flag==0):
-         print("No bus today")
+     x=int(float(systime[1])/15)
+     if int(systime[0])>=22 or int(systime[0])<7 :
+        print("next bus "+"07:15")
+     else :
+        if(x==0):
+          print("next bus "+systime[0]+":15")
+        elif x==1:
+          print("next bus "+systime[0]+":30")
+        elif x==2:
+          print("next bus "+systime[0]+":45")
+        else :
+          print("next bus "+str(int(systime[0])+1)+":15")
